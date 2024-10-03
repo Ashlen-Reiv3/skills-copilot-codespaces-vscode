c54b4069e17e5cae5bc797666c9d4ada36263046
Ashlen-Reiv3 <47207461+Ashlen-Reiv3@users.noreply.github.com>
2024-10-03 06:03:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller:'SkillsMemberController',
+        controllerAs:'vm',
+        bintToController: true,
+        scope: {member:'m'}
+    };
+}
